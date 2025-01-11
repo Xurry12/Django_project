93654a9c5546cc1b6fc1327da3dfd35886514d5a
Xurry <ivan.chernakov.ekb@gmail.com>
2025-01-11 17:37:33 +0500

    Восьмой коммит

--- ulearnProject/settings.py
+++ ulearnProject/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = 'django-insecure-97gzpnu%9sskb_7(dml#^ph#t3**7$(ot@1ipnl5n7(d1r@x)1
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
